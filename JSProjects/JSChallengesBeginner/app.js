276d25a4a97b9af61945773020c4426f4e845c0d
Aaqeeb Hussain <24283167+aaqeebh1@users.noreply.github.com>
2022-07-11 00:09:32 +0100

    JSchallenges beginner complete

---
+++ JSProjects/JSChallengesBeginner/app.js
@@ -0,0 +1,78 @@
+// function addition(num1, num2) {
+//     return num1 + num2
+// }
+
+// console.log(addition(7,3))
+//
+
+// function hoursIntoSeconds(hours) {
+//     return hours * 60 * 60
+// }
+
+// console.log(hoursIntoSeconds())
+
+// function calcPerimeter(length, width) {
+//     return (length * 2) + (width * 2)
+// }
+
+// console.log(calcPerimeter(20,10))
+
+// function calcTriangleArea(base, height) {
+//   return 0.5 * base * height;
+// }
+
+// console.log(calcTriangleArea(10, 10));
+
+// function appendFrontend (str) {
+//     return str + 'Frontend'
+// }
+
+// console.log(appendFrontend('Banana'))
+
+// function sumGreaterThan100(num1, num2) {
+//   return num1 + num2 > 100
+// }
+
+// console.log(sumGreaterThan100(20, 10));
+
+// function lessThanOrEqualTo(num) {
+//   return num <= 0
+// }
+
+// console.log(lessThanOrEqualTo(-3));
+
+// function returnOpposite(bool) {
+//     return !bool
+// }
+
+// console.log(returnOpposite(false))
+
+// function isNotZero(num) {
+//     return num !== 0
+// }
+
+// console.log(isNotZero(-6))
+
+// function calcRemainder(num1, num2) {
+//     return num1 % num2
+// }
+
+// console.log(calcRemainder(9,8))
+
+// function isOdd(num) {
+//     return num % 2 === 1
+// }
+
+// console.log(isOdd())
+
+// function booleanInteger(num) {
+//     return num % 2 === 0 ? 1 : -1
+// }
+
+// console.log(booleanInteger())
+
+// function isLoggedInAndSubscribed(str1, str2) {
+//     return str1 === 'LOGGED_IN' && str2 === 'SUBSCRIBED'
+// }
+
+// console.log(isLoggedInAndSubscribed('LOGGED_OUT', 'SUBSCRIBED'))
