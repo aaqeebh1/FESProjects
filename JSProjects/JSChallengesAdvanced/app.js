e43dd507bf1047cb3daf2608d45de813cdcb8f03
Aaqeeb Hussain <24283167+aaqeebh1@users.noreply.github.com>
2022-07-12 22:15:39 +0100

    JSChallenges Advanced 3 q's complete

---
+++ JSProjects/JSChallengesAdvanced/app.js
@@ -0,0 +1,46 @@
+// question 1
+
+// function showRating(rating){
+
+//     //  for loop
+//     let ratings = ""
+//     for (let i = 0; i < Math.floor(rating); ++i){
+//        if(i === Math.floor(rating) - 1){
+//            ratings += "*"
+//        } else {
+//         ratings += "* "
+//        }
+//     }
+//     if (!Number.isInteger(rating)){
+//         ratings += " ."
+//     }
+//     return ratings
+
+// }
+
+// console.log(showRating(6.5))
+
+//  question 2
+
+// function sortLowToHigh(arr){
+//     return arr.sort((a, b) => a - b)
+// }
+
+// console.log(sortLowToHigh([1,5,8,9,251,3,7,1,4,5,6]))
+
+// question 3
+
+// function sortHighToLow(obj) {
+//   return obj.sort((a, b) =>  b.price - a.price);
+// }
+
+// console.log(
+//   sortHighToLow([
+//     { id: 1, price: 50 },
+//     { id: 2, price: 0 },
+//     { id: 3, price: 500 },
+//   ])
+// );
+
+//  question 4 
+
