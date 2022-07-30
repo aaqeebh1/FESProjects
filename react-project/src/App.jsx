5fa1f2402ba632bcfb7f0ae5bfc92d90858ea1f9
Aaqeeb Hussain <24283167+aaqeebh1@users.noreply.github.com>
2022-07-30 15:02:43 +0100

    Setting up react

---
+++ react-project/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Nav from './Nav';
+
+function App() {
+  return (
+    <div className="App">
+     Aaqeeb Hussain
+     <Nav />
+    </div>
+  );
+}
+
+export default App;
